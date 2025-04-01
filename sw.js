21e6c7447486c52f3a58bad008b8acf53d05c77d
George Liu <eva2000@centminmod.com>
2025-04-02 09:09:30 +1000

    update PWA

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 // --- Configuration ---
 // Increment this version number when you update any assets in urlsToCache
-const APP_VERSION = 'v11';
+const APP_VERSION = 'v12';
 const CACHE_NAME = `missile-command-cache-${APP_VERSION}`;
 
 // List of assets to cache on installation
