1d228d99e51f78f122f1473a2d67594553875bc1
George Liu <eva2000@centminmod.com>
2025-04-01 23:08:36 +1000

    add plane bombing runs

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 // --- Configuration ---
 // Increment this version number when you update any assets in urlsToCache
-const APP_VERSION = 'v7';
+const APP_VERSION = 'v8';
 const CACHE_NAME = `missile-command-cache-${APP_VERSION}`;
 
 // List of assets to cache on installation
