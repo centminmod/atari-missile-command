addca733918868f83c4ec82dfd9cffc86b1dfeca
George Liu <eva2000@centminmod.com>
2025-04-04 06:27:03 +1000

    update leaderboard font size

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 // --- Configuration ---
 // Increment this version number when you update any assets in urlsToCache
-const APP_VERSION = 'v43';
+const APP_VERSION = 'v44';
 const CACHE_NAME = `missile-command-cache-${APP_VERSION}`;
 
 // List of assets to cache on installation
