30fa1e4aa06f12359f60f33de8b0770144208157
George Liu <eva2000@centminmod.com>
2025-04-03 11:51:57 +1000

    increase input box size

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 // --- Configuration ---
 // Increment this version number when you update any assets in urlsToCache
-const APP_VERSION = 'v35';
+const APP_VERSION = 'v36';
 const CACHE_NAME = `missile-command-cache-${APP_VERSION}`;
 
 // List of assets to cache on installation
