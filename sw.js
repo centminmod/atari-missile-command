b3e3ffb007c429a2e6d86af5a1176110eced378c
George Liu <eva2000@centminmod.com>
2025-04-05 22:53:42 +1000

    improve screenshot function part 2

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 // --- Configuration ---
 // Increment this version number when you update any assets in urlsToCache
-const APP_VERSION = 'v66';
+const APP_VERSION = 'v67';
 const CACHE_NAME = `missile-command-cache-${APP_VERSION}`;
 
 // List of assets to cache on installation
